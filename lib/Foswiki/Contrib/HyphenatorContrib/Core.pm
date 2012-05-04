26bedf6403c84bb0284b628ae37f17faf7d43149
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/HyphenatorContrib@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/HyphenatorContrib/Core.pm
+++ lib/Foswiki/Contrib/HyphenatorContrib/Core.pm
@@ -30,23 +30,23 @@ This is the perl stub for the hyphenator plugin.
 =cut
 
 sub new {
-  my $class = shift;
-  my $session = shift || $Foswiki::Plugins::SESSION;
- 
-  my $this = bless(
-    $class->SUPER::new(
-      $session,
-      name => 'Hyphenator',
-      version => '4.0.0',
-      author => 'Mathias Nater',
-      homepage => 'http://code.google.com/p/hyphenator/',
-      javascript => ['hyphenator.js', 'hyphenator.init.js'],
-      puburl => '%PUBURLPATH%/%SYSTEMWEB%/HyphenatorContrib',
-    ),
-    $class
-  );
+    my $class = shift;
+    my $session = shift || $Foswiki::Plugins::SESSION;
 
-  return $this;
+    my $this = bless(
+        $class->SUPER::new(
+            $session,
+            name       => 'Hyphenator',
+            version    => '4.0.0',
+            author     => 'Mathias Nater',
+            homepage   => 'http://code.google.com/p/hyphenator/',
+            javascript => [ 'hyphenator.js', 'hyphenator.init.js' ],
+            puburl     => '%PUBURLPATH%/%SYSTEMWEB%/HyphenatorContrib',
+        ),
+        $class
+    );
+
+    return $this;
 }
 
 1;
